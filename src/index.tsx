62236536c135f09c5fb48f7176d5ccf531473241
wanzhs <wanzhs_cedar@163.com>
2020-11-06 19:39:05 +0800

    项目初始化 mockjs 启用控制

--- src/index.tsx
+++ src/index.tsx
@@ -1,38 +1,37 @@
 import React from 'react'
 import dva from 'dva'
-import { BrowserRouter as Router, HashRouter } from 'react-router-dom'
+import {BrowserRouter as Router, HashRouter} from 'react-router-dom'
 import createLoading from 'dva-loading'
-import { DraggableModalProvider as ModalProvider } from '@components/common/ant-design-draggable-modal'
-import './scripts/mock'
+import {DraggableModalProvider as ModalProvider} from '@components/common/ant-design-draggable-modal'
 import '@styles/sass/global.scss'
 import 'react-placeholder/lib/reactPlaceholder.css'
 
 import history from '@scripts/history'
 import global from '@store/models/global'
+import {RenderRouter, ThemeProvider} from '@components/common'
+import {ReactRouterGlobalHistory} from '@components/common/GlobalHistory'
+import {isMock} from "@root/src/config/config.constant";
 // import registerServiceWorker from './registerServiceWorker'
 
-import { RenderRouter, ThemeProvider } from '@components/common'
-import { ReactRouterGlobalHistory } from '@components/common/GlobalHistory'
-
-const { isProd } = window.g_config
-const app = dva({ history })
-export const ModeRouter: React.FC = ({ children }) =>
-  isProd ? <HashRouter>{children}</HashRouter> : <Router>{children}</Router>
-
+const {isProd} = window.g_config
+const app = dva({history})
+export const ModeRouter: React.FC = ({children}) =>
+    isProd ? <HashRouter>{children}</HashRouter> : <Router>{children}</Router>
+isMock && require('./scripts/mock');
 window.g_app = app
 app.use(createLoading())
 app.model(global)
 app.router(props => {
-  return (
-    <ModeRouter>
-      <ThemeProvider>
-        <ModalProvider>
-          <ReactRouterGlobalHistory />
-          <RenderRouter {...props} />
-        </ModalProvider>
-      </ThemeProvider>
-    </ModeRouter>
-  )
+    return (
+        <ModeRouter>
+            <ThemeProvider>
+                <ModalProvider>
+                    <ReactRouterGlobalHistory/>
+                    <RenderRouter {...props} />
+                </ModalProvider>
+            </ThemeProvider>
+        </ModeRouter>
+    )
 })
 app.start('#root')
 
